0db9d06ab150d7e384d6a3e082a56bc28a72ec14
iamchuckss <s2011297@gmail.com>
2019-07-13 15:23:19 -0500

    construct basic project structure

---
+++ src/containers/recipes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Recipes extends Component {
+    render() {
+        return (
+            <div className="Recipes">
+                No recipes yet
+            </div>
+        )
+    ;}
+}
+
+export default Recipes;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Recipes from './containers/recipes';
+import './assets/css/styles.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <div>
+            <Recipes />
+        </div>
+    </Provider>
+    , document.getElementById('root')
+);

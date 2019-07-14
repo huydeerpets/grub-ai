71646af19111742941ace9829e0aa1f40bbd4e2d
iamchuckss <s2011297@gmail.com>
2019-07-14 15:53:03 -0500

    remove console log for testing

--- src/reducers/recipe_reducer.js
+++ src/reducers/recipe_reducer.js
@@ -5,7 +5,6 @@ const initialState = {};
 export default (state = initialState, action) => {
   switch (action.type) {
     case types.FETCH_RECIPES:
-      console.log(action.payload)
         return {
           ...state,
           allRecipes: action.payload,
